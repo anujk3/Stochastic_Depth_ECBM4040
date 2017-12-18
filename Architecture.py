38cdbaca30e494c873b9b4516c48966bf659e484
aggvarun01 <varun.aggarwal@columbia.edu>
2017-12-17 22:44:36 -0500

    final code

--- Architecture.py
+++ Architecture.py
@@ -6,16 +6,13 @@
 
 import tensorflow as tf
 import numpy as np
-from layers import architecture, evaluate
+from layers import architecture, loss_function
 from utils.cifar_utils import load_data
 import time
+from datetime import datetime
 
 
-# In[ ]:
-
-
-start_time = time.time()
-
+# # Data Input
 
 # In[ ]:
 
@@ -24,138 +21,96 @@ start_time = time.time()
 X_train, y_train = load_data(mode='train')
 X_test, y_test = load_data(mode='test')
 
-# shuffle_indices = np.arange(45000)
-# np.random.shuffle(shuffle_indices,)
-# X_train = X_train[shuffle_indices]
-# y_train = y_train[shuffle_indices]
-
-# Data organizations:
-# Train data: 49000 samples from original train set: 1~49000
-# Validation data: 1000 samples from original train set: 49000~50000
-# num_training = 45000
-# num_validation = 5000
-
-# X_val = X_train[-num_validation:, :]
-# y_val = y_train[-num_validation:]
-
-# X_train = X_train[:num_training, :]
-# y_train = y_train[:num_training]
-
 
 # In[ ]:
 
 
-# print('y_val counts: ', np.bincount(y_val))
-
-
-# In[ ]:
-
-
-# X_train = X_train[:256]
-# y_train = y_train[:256]
-
-
-# In[ ]:
-
-
-# Preprocessing: subtract the mean value across every dimension for training data, and reshape it to be RGB size
+# Preprocessing: subtract the mean value across every dimension for training data
 mean_image = np.mean(X_train, axis=0)
 std_image = np.std(X_train, axis=0)
 
 X_train = (X_train.astype(np.float32) - mean_image.astype(np.float32)) / std_image
-# X_val = (X_val.astype(np.float32) - mean_image) / std_image
 X_test = (X_test.astype(np.float32) - mean_image) / std_image
 
 
 # In[ ]:
 
 
+# Reshape it to be size NHWC
 X_train = X_train.reshape([X_train.shape[0],3,32,32]).transpose((0,2,3,1))
-# X_val = X_val.reshape([X_val.shape[0],3,32,32]).transpose((0,2,3,1))
 X_test = X_test.reshape([X_test.shape[0],3,32,32]).transpose((0,2,3,1))
 
 print('Train data shape: ', X_train.shape)
 print('Train labels shape: ', y_train.shape)
-# print('Validation data shape: ', X_val.shape)
-# print('Validation labels shape: ', y_val.shape)
 print('Test data shape', X_test.shape)
 print('Test labels shape', y_test.shape)
 
 
-# # change ADAM?
-# # Use better initializations for batch_norm?
-# # add model saver/checkpointer. In addition keep track of best validation error and save that model
+# # Declare tensorflow variables and architecture
 
 # In[ ]:
 
 
-inputs = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3])
+X_inputs = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3])
 random_rolls = tf.placeholder(dtype=tf.float32, shape=[54])
 y_inputs = tf.placeholder(dtype=tf.uint8, shape=[None])
 is_training = tf.placeholder(dtype=tf.bool)
 y_test_tf = tf.placeholder(dtype=tf.int64, shape=[None])
 test_error_tf = tf.placeholder(dtype=tf.float32, shape=())
 
-lr = 0.1
+train_size = len(X_train)
+test_size = len(X_test)
+epochs = 500
+batch_size = 128
+boundaries = [int(.5 * epochs * train_size / batch_size), int(.75 * epochs * len(X_train) / batch_size)]
+values = [.1, .01, .001]
 weight_decay = 1e-4
 momentum = 0.9
-batch_size = 128
-train_size = len(X_train)
-epochs = 500
+shuffle_indices = np.arange(train_size)
 
-out = architecture(inputs, random_rolls, is_training, strategy='pad')
 labels = tf.one_hot(y_inputs, 10)
-softmax_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out, labels=labels))
-# confirm this is right
-regularizer_loss = weight_decay * tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables('stoch_depth')
-                                            if 'weights' in var.name])
-loss = softmax_loss + regularizer_loss
+
+out = architecture(X_inputs, random_rolls, is_training, strategy='pad',
+                   scope='stoch_depth', P=0.5, L=54)
+
+loss = loss_function(logits=out, labels=labels, weight_decay=weight_decay,
+                     arch_scope='stoch_depth')
 training_summary = tf.summary.scalar('train_loss', loss)
-step = tf.train.MomentumOptimizer(lr, momentum, use_nesterov=True).minimize(loss)
-# step = tf.train.AdamOptimizer(lr).minimize(loss)
-# eve = evaluate(out, y_val)
-# eve = evaluate(out, y_test)
+
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.piecewise_constant(global_step, boundaries, values)
+step = tf.train.MomentumOptimizer(learning_rate, momentum, use_nesterov=True).minimize(loss, global_step=global_step)
 pred = tf.argmax(out, axis=1)
 error_num = tf.count_nonzero(pred - y_test_tf, name='error_num')
-# validation_summary = tf.summary.scalar('Stoch_error', 100 * error_num / batch_size)
 validation_summary = tf.summary.scalar('Test_Error', test_error_tf)
 
 
+# # Create unique log file name
+
 # In[ ]:
 
 
-from datetime import datetime
 now = datetime.now()
 datetime_str = now.strftime("%Y%m%d-%H%M%S")
-# datetime_str = "20171214-060557"
 logdir = "tf_logs/{}/".format(datetime_str)
 
 
-# In[ ]:
-
-
-test_size = len(X_test)
-
+# # Train the network
 
 # In[ ]:
 
 
-best_val = np.inf
 with tf.Session() as sess: 
     
-#     merge = tf.summary.merge_all()
     writer = tf.summary.FileWriter(logdir, sess.graph)
     saver = tf.train.Saver()
     sess.run(tf.global_variables_initializer())
-#     saver.restore(sess, "checkpoints/{}/best_validation.ckpt".format(datetime_str))
     start_index = 0
-#     iter_number = 30340
     iter_number = -1
-#     for epoch in range(87, epochs):
     for epoch in range(epochs):
         epoch_start_time = time.time()
-        if epoch in (250, 375):
-            lr /= 10
+        np.random.shuffle(shuffle_indices)
+        X_train, y_train = X_train[shuffle_indices], y_train[shuffle_indices]
         for _ in range(train_size // batch_size):
             iter_number += 1
             if start_index + batch_size >= train_size:
@@ -177,7 +132,7 @@ with tf.Session() as sess:
                     X_train_batch[inx] = np.flip(X_train_batch[inx], axis=1)
 
             random_rolls_batch = np.random.uniform(size=54)
-            _, loss_val, train_summ = sess.run([step, loss, training_summary], feed_dict={inputs: X_train_batch,
+            _, loss_val, train_summ = sess.run([step, loss, training_summary], feed_dict={X_inputs: X_train_batch,
                                                                                           random_rolls: random_rolls_batch,
                                                                                           y_inputs: y_train_batch,
                                                                                           is_training: True})
@@ -189,12 +144,8 @@ with tf.Session() as sess:
             start_index_test += 100
 
             X_test_batch, y_test_batch = X_test[indices_test], y_test[indices_test]
-            
-#             val_summ = sess.run(validation_summary, feed_dict={inputs: X_test_batch,
-#                                                                random_rolls: random_rolls_batch,
-#                                                                y_test_tf: y_test_batch,
-#                                                                is_training: False})
-            test_batch_accuracy = sess.run(error_num, feed_dict={inputs: X_test_batch,
+
+            test_batch_accuracy = sess.run(error_num, feed_dict={X_inputs: X_test_batch,
                                                                  random_rolls: random_rolls_batch,
                                                                  y_test_tf: y_test_batch,
                                                                  is_training: False})
@@ -206,80 +157,7 @@ with tf.Session() as sess:
         if epoch % 10 == 0:
             save_path = saver.save(sess, 'checkpoints/{}/model.ckpt'.format(datetime_str))
             print("Model saved in file: %s" % save_path)
-#         if val < best_val:
-#             best_val = val
-#             save_path = saver.save(sess, 'checkpoints/{}/best_validation.ckpt'.format(datetime_str))
-#             print("Best validation model saved in file: %s" % save_path)
         print("epoch_time =", epoch_start_time - time.time())
             
     writer.close()
-
-
-# In[ ]:
-
-
-print(time.time() - start_time)
-
-
-# # Tests (ignore)
-
-# In[ ]:
-
-
-# # confirm tensorboard
-# random_rolls_batch = np.random.uniform(size=54)
-# X_train_batch = X_train[list(range(20))]
-# with tf.Session() as sess:
-#     writer = tf.summary.FileWriter('logs', sess.graph)
-#     sess.run(tf.global_variables_initializer())
-#     sess.run(out, feed_dict={inputs: X_train_batch,
-#                              random_rolls: random_rolls_batch,
-#                              is_training: True})
-#     sess.run(out, feed_dict={inputs: X_val,
-#                              random_rolls: random_rolls_batch,
-#                              is_training: False})
-#     writer.close()
-
-
-# In[ ]:
-
-
-# confirm training = False gives same answers
-# X_train_batch = X_train[:20]
-# with tf.Session() as sess:
-#     ls = []
-#     for i in range(3):
-#         random_rolls_batch = np.random.uniform(size=54)
-#         if i == 0:
-#             sess.run(tf.global_variables_initializer())
-#         ls.append(sess.run(out, feed_dict={inputs: X_train_batch,
-#                                            random_rolls: random_rolls_batch,
-#                                            is_training: True}))
-
-
-# In[ ]:
-
-
-# # confirm training = False but new init gives diff answers
-# with tf.Session() as sess:
-#     ls = []
-#     with tf.variable_scope('stoch_depth', reuse=tf.AUTO_REUSE):
-#         for i in range(3):
-#             out = architecture(inputs, False)
-#             sess.run(tf.global_variables_initializer())
-#             ls.append(sess.run(out, feed_dict={inputs: X}))
-
-
-# In[ ]:
-
-
-# confirm training = True gives diff answers
-# with tf.Session() as sess:
-#     ls = []
-#     with tf.variable_scope('stoch_depth', reuse=tf.AUTO_REUSE):
-#         for i in range(3):
-#             out = architecture(inputs, True)
-#             if i == 0:
-#                 sess.run(tf.global_variables_initializer())
-#             ls.append(sess.run(out, feed_dict={inputs: X}))
 
--- layers.py
+++ layers.py
@@ -1,12 +1,24 @@
 import tensorflow as tf
 
-# W=(W−F+2P)/S+1
 
+def first_layer(inputs, is_training, num_outputs=16, scope='Inputs'):
+    '''
+    conv2d -> batch_norm -> relu
 
-def first_layer(inputs, is_training, scope):
+    Parameters
+    ----------
+    inputs: input tensor with shape "NHWC"
+    is_training: boolean tensor to indicate training/testing mode
+    num_outputs: (default: 16) output depth
+    scope: (default: "Inputs") variable scope
+
+    Output
+    ------
+    tensor of shape "NHWC" where C=num_outputs
+    '''
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
         out = tf.contrib.layers.conv2d(inputs,
-                                       num_outputs=16,
+                                       num_outputs=num_outputs,
                                        kernel_size=(3, 3),
                                        stride=1,
                                        padding='SAME',
@@ -20,6 +32,21 @@ def first_layer(inputs, is_training, scope):
 
 def conv_block(inputs, output_size, first_kernel_size, first_stride,
                first_padding, is_training):
+    '''
+    conv2d -> batch_norm -> relu -> conv2d -> batch_norm
+
+    Creates block with two back to back convolution layers.
+    This is the building block for a single residual block.
+
+    Parameters
+    ----------
+    inputs: input tensor with shape "NHWC"
+    output_size: number of output channels
+    first_kernel_size: usually 3x3, except in transition block, where it is 2x2
+    first_stride: usually 1, except in transition block, where it is 2
+    first_padding: usually "SAME", except in transition block, where it is "VALID"
+    is_training: tensorflow boolean indicating wheter training or testing mode
+    '''
     conv = tf.contrib.layers.conv2d(inputs,
                                     num_outputs=output_size,
                                     kernel_size=first_kernel_size,
@@ -31,7 +58,6 @@ def conv_block(inputs, output_size, first_kernel_size, first_stride,
                                                        'is_training': is_training,
                                                        'scale': True})
 
-    # try changing initializer for gamma in batch_norm for this layer
     conv = tf.contrib.layers.conv2d(conv,
                                     num_outputs=output_size,
                                     kernel_size=(3, 3),
@@ -47,6 +73,28 @@ def conv_block(inputs, output_size, first_kernel_size, first_stride,
 
 def residual_block(inputs, output_size, survival_rate, random_roll,
                    is_training, scope):
+    '''
+    -> conv_block -> + -> relu
+      |              ^
+      |_ _ _ _ _ _ _ |
+
+    Basic residual block with stochastic depth. During testing, 
+    the layers are not dropped. Instead, the output is multiplied
+    by the survival rate.
+
+    Parameters
+    ----------
+    inputs: input tensor of shape "NHWC"
+    output_size: number of output channels (same as input channels)
+    survival_rate: scalar defining the survival rate of the layer
+    random_roll: random number between [0, 1) generated every epoch
+    is_training: boolean specifying training/testing mode
+    scope: string to specify variable scope
+
+    Output
+    ------
+    tensor with shape "NHWC"
+    '''
 
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
 
@@ -56,8 +104,10 @@ def residual_block(inputs, output_size, survival_rate, random_roll,
 
         conv = tf.cond(tf.logical_and(is_training, tf.logical_not(survives)),
                        lambda: tf.zeros_like(identity),
-                       lambda: conv_block(inputs, output_size, (3, 3), 1, "SAME",
-                                          is_training),
+                       lambda: conv_block(inputs, output_size,
+                                          first_kernel_size=(3, 3),
+                                          first_stride=1, first_padding="SAME",
+                                          is_training=is_training),
                        name='first_cond')
         conv = tf.cond(is_training,
                        lambda: conv,
@@ -68,14 +118,42 @@ def residual_block(inputs, output_size, survival_rate, random_roll,
 
 
 def transition_block(inputs, output_size, survival_rate, random_roll,
-                     is_training, scope, strategy='conv'):
+                     is_training, scope, strategy='pad'):
+    '''
+    -> conv_block - - - - > + -> relu
+      |                     ^
+      |                     |
+       -> avg_pool -> pad ->
+
+    Modified residual block. This is the first cell of the second
+    and third stacks. This halves the image width and height, but
+    doubles the number of channels, thus maintaining time complexity.
+    During testing, the layer is not dropped. Instead, the output is
+    multiplies by the survival rate
+
+    Parameters
+    ----------
+    inputs: input tensor of shape "NHWC"
+    output_size: number of output channels (double of input channels)
+    survival_rate: scalar defining the survival rate of the layer
+    random_roll: random number between [0, 1) generated every epoch
+    is_training: boolean specifying training/testing mode
+    scope: string to specify variable scope
+    strategy: 'pad' or 'conv'. method to follow to increase the number of
+              channels of identity connection. Iriginal paper uses 'pad'
+
+    Output
+    ------
+    tensor with shape "NHWC" where H and W are half of original imahge
+    and C is double
+    '''
 
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
         identity = tf.contrib.layers.avg_pool2d(inputs,
                                                 kernel_size=(2, 2),
                                                 stride=2,
                                                 padding='VALID')
-        # confirm. Anuj may be right. seems like they did zero padding instead of this.
+
         if strategy == 'conv':
             identity = tf.contrib.layers.conv2d(identity,
                                                 num_outputs=output_size,
@@ -95,8 +173,11 @@ def transition_block(inputs, output_size, survival_rate, random_roll,
 
         conv = tf.cond(tf.logical_and(is_training, tf.logical_not(survives)),
                        lambda: tf.zeros_like(identity),
-                       lambda: conv_block(inputs, output_size, (2, 2), 2, "VALID",
-                                          is_training),
+                       lambda: conv_block(inputs, output_size,
+                                          first_kernel_size=(2, 2),
+                                          first_stride=2,
+                                          first_padding="VALID",
+                                          is_training=is_training),
                        name='first_cond')
 
         conv = tf.cond(is_training,
@@ -107,8 +188,15 @@ def transition_block(inputs, output_size, survival_rate, random_roll,
         return tf.nn.relu(conv + identity)
 
 
-# output layer could be the issue?
 def output_layer(inputs, scope, output_size=10):
+    '''
+    -> avg_pool -> flatten -> fully_connected
+    Parameters
+    ----------
+    inputs: input tensor of shape "NHWC"
+    scope: string to define variable scope
+    output_size: 10 for CIFAR-10
+    '''
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
         out = tf.contrib.layers.avg_pool2d(inputs,
                                            kernel_size=inputs.shape[1:3],
@@ -121,45 +209,86 @@ def output_layer(inputs, scope, output_size=10):
     return out
 
 
-def architecture(inputs, random_rolls, is_training, strategy, P=0.5, L=54):
-    with tf.variable_scope('stoch_depth', reuse=tf.AUTO_REUSE):
+def architecture(inputs, random_rolls, is_training, strategy, P=0.5, L=54,
+                 scope='stoch_depth'):
+    '''
+    Builds the stochastic depth network. Current implementation consists of 3 stacks
+    of 18 residual blocks each. Thus, counting the 2 convolution layers per residual
+    block, the input layer and the output layer, the total number of layers comes
+    out to be 110.
 
-        out = first_layer(inputs, is_training, 'input')
+    Inputs
+    -----
+    inputs: input tensor of shape NHWC
+    random_rolls: a tensor of shape (L,) where each value is a number from [0, 1),
+                  randomly generated for each mini-batch
+    is_training: boolean to indicate training/testing mode
+    strategy: 'pad' or 'conv'. Strategy to match dimensions of the identity function
+              in the transition block. The paper uses 'pad'
+    P: The probability of survival of the input layer
+    L: 
+    '''
+    num_stacks = 3
+    assert L % num_stacks == 0
+    res_per_stack = L // num_stacks
 
-        l = 1
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+
+        out = first_layer(inputs, is_training)
+
+        survival_rates = [1 - l / L * (1 - P) for l in range(1, L+1)]
+
+        l = 0
         with tf.variable_scope('stack1', reuse=tf.AUTO_REUSE):
-            for i in range(1, 19):
-                p = 1 - l / L * (1 - P)
-                out = residual_block(out, 16, p, random_rolls[l-1], is_training, 'res'+str(i))
+            for i in range(res_per_stack):
+                out = residual_block(inputs=out, output_size=16, survival_rate=survival_rates[l],
+                                     random_roll=random_rolls[l], is_training=is_training,
+                                     scope='res'+str(i))
                 l += 1
 
         with tf.variable_scope('stack2', reuse=tf.AUTO_REUSE):
-            p = 1 - l / L * (1 - P)
-            out = transition_block(out, 32, p, random_rolls[l-1], is_training, 'res'+str(1), strategy)
+            out = transition_block(inputs=out, output_size=32, survival_rate=survival_rates[l],
+                                   random_roll=random_rolls[l], is_training=is_training,
+                                   scope='res'+str(0), strategy=strategy)
             l += 1
 
-            for i in range(2, 19):
-                p = 1 - l / L * (1 - P)
-                out = residual_block(out, 32, p, random_rolls[l-1], is_training, 'res'+str(i))
+            for i in range(1, res_per_stack):
+                out = residual_block(inputs=out, output_size=32, survival_rate=survival_rates[l],
+                                     random_roll=random_rolls[l], is_training=is_training,
+                                     scope='res'+str(i))
                 l += 1
 
         with tf.variable_scope('stack3', reuse=tf.AUTO_REUSE):
-            p = 1 - l / L * (1 - P)
-            out = transition_block(out, 64, p, random_rolls[l-1], is_training, 'res'+str(1), strategy)
+            out = transition_block(inputs=out, output_size=64, survival_rate=survival_rates[l],
+                                   random_roll=random_rolls[l], is_training=is_training,
+                                   scope='res'+str(0), strategy=strategy)
             l += 1
 
-            for i in range(2, 19):
-                p = 1 - l/L * (1 - P)
-                out = residual_block(out, 64, p, random_rolls[l-1], is_training, 'res'+str(i))
+            for i in range(1, res_per_stack):
+                out = residual_block(inputs=out, output_size=64, survival_rate=survival_rates[l],
+                                     random_roll=random_rolls[l], is_training=is_training,
+                                     scope='res'+str(i))
+                l += 1
 
-        out = output_layer(out, 'out', 10)
+        out = output_layer(out, scope='out', output_size=10)
 
         return out
 
 
-def evaluate(output, input_y):
-    with tf.name_scope('evaluate'):
-        output = tf.nn.softmax(output)
-        pred = tf.argmax(output, axis=1)
-        error_num = tf.count_nonzero(pred - input_y, name='error_num')
-    return error_num
+def loss_function(logits, labels, weight_decay, arch_scope):
+    '''
+    Calculates softmax_loss and adds l2 regularizer on the weights
+
+    Parameters
+    ----------
+    logits: tensor representing unnormalized decision function for each class
+    labels: one hot encoded true labels
+    weight_decay: scalar representing regularization constant
+    arch_scope: scope name of the full architecture. Required to determine all
+                the trainable parameters in the architecture
+    '''
+    softmax_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,
+                                                                          labels=labels))
+    regularizer_loss = weight_decay * tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables(arch_scope)
+                                                if 'weights' in var.name])
+    return softmax_loss + regularizer_loss
